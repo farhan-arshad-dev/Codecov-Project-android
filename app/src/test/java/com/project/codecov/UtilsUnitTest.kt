ab6f80befd963d44176c0cfb6f8fdb46a7b24223
FarhanArshad <farhanarshad@A006-00861.local>
2022-09-22 13:48:31 +0500

    Integrate jacoco test reporting

--- app/src/test/java/com/project/codecov/UtilsUnitTest.kt
+++ app/src/test/java/com/project/codecov/UtilsUnitTest.kt
@@ -1,17 +1,26 @@
 package com.project.codecov
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class UtilsUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun isOddNumberTest() {
+        assertEquals(true, Utils.isOddNumber(97))
+    }
+
+    @Test
+    fun isEvenNumberTest() {
+        assertEquals(false, Utils.isOddNumber(88))
     }
 }
